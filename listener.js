baebd24898bb3f87bb383d5e683e784f656ed119
Diuxx <nicolasmarmot@gmail.com>
2021-01-07 14:53:11 +0100

    feat(cache): add manifext.appcache

--- listener.js
+++ listener.js
@@ -1,7 +1,20 @@
 $(function () {
     // --
 
-
+// On vérifie si un nouveau cache est disponible au chargement de la page
+    window.addEventListener('load', function(e) {
+        window.applicationCache.addEventListener('updateready', function(e) {
+            if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+                // Là, le navigateur a téléchargé un nouveau cache
+                // On change le cache et on recharge la page
+                window.applicationCache.swapCache();
+                window.location.reload();
+            } else {
+                // Le cache Manifest n'a pas changé, on ne fait rien
+                console.log('cache manifest nochanges detected..');
+            }
+        }, false);
+    }, false);
 
 
     /**
